34562ef67411547659a9bfb390c028b75a6d5878
wenzhuo4657 <14783149521@163.com>
2024-11-01 08:44:56 +0800

    feat:活动api迭代

---
+++ big-market/build.sh
@@ -0,0 +1,1 @@
+docker build -t  jacka7732/big-market-front-app:1.0 .
--- big-market/src/app/page.tsx
+++ big-market/src/app/page.tsx
@@ -4,13 +4,15 @@ import {LuckyWheelPage} from "@/app/pages/lucky/lucky-wheel-page";
 import {LuckyGridPage} from "@/app/pages/lucky/lucky-grid-page";
 import dynamic from "next/dynamic";
 
-const StrategyArmoryButton = dynamic(async()=>(await import("./pages/components/StrategyArmory")).StrategyArmory)
+const StrategyArmoryButton = dynamic(async () => (await import("./components/StrategyArmory")).StrategyArmory)
 
 export default function Home() {
+
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100"
+             style={{backgroundImage: "url('/background.svg')", backgroundColor:"#e7305e;"}}>
             {/* 头部文案 */}
-            <header className="text-3xl font-bold text-center text-gray-800 my-8">
+            <header className="text-7xl font-bold text-center text-gray-800 my-8" style={{color:"white"}}>
                 大营销平台 - 抽奖展示
             </header>
 
@@ -32,9 +34,9 @@ export default function Home() {
             </div>
 
             {/* 底部文案 */}
-            <footer className="text-gray-600 text-center my-8">
-                该项目完全自研
+            <footer className="text-gray-600 text-center my-8" style={{color:"white"}}>
+                ---------
             </footer>
         </div>
-  );
+    );
 }
