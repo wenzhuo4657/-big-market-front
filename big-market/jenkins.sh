dd79fae3e95e172b93510b0a1245761f7b4b4eab
wenzhuo4657 <14783149521@163.com>
2024-11-05 17:55:29 +0800

    dev-ops

---
+++ big-market/jenkins.sh
@@ -0,0 +1,14 @@
+# 先删除之前的容器和镜像文件
+if [ "$(docker ps -a | grep xfg-dev-tech-app)" ]; then
+docker stop xfg-dev-tech-app
+docker rm xfg-dev-tech-app
+fi
+if [ "$(docker images -q jacka7732/big-market-front-app)" ]; then
+docker rmi jacka7732/big-market-front-app
+fi
+
+
+# 重新生成
+cd /var/jenkins_home/workspace/big-market
+docker build -t  jacka7732/big-market-front-app:1.0 .
+docker run -itd -p 3000:3000 --name xfg-dev-tech-app jacka7732/big-market-front-app:1.0
